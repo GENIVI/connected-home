1bd1ec84ff730c2c8872781aab4733a9ffd32f1e
Justin Noel <justin@ics.com>
2016-10-06 20:35:21 -0400

    Added icon, app manifest and service files for GDP Launcher integration Added deployment instructions to aid yocto integration

---
+++ com.genivi.gdp.connectedhome.app
@@ -0,0 +1,4 @@
+Name=Connected Home
+Icon=file://opt/com.genivi.gdp.connectedhome/share/icons/com.genivi.gdp.connectedhome.svg
+Unit=com.genivi.gdp.connectedhome
+Exec=/opt/com.genivi.gdp.connectedhome/bin/qt-smart-demo
--- main.cpp
+++ main.cpp
@@ -1,22 +1,5 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-//#include <QQuickView>
-
-//
-// Define IVI surface for application
-//
-// Use a well-known surface ID from an existing POC app
-// or ensure your surface ID is known to the Layer Manager
-//
-// Known surface IDs & corresponding PoC apps:
-// 3    - QML Example
-// 20   - AudioManager Monitor
-// 30   - Browser PoC
-// 40   - Fuel Service Advisor PoC
-// 10   - Mock Navigation PoC
-// 5100 - MediaPlayer PoC
-
-#define CONNECTED_HOME_SURFACE_ID 10
 
 int main(int argc, char *argv[])
 {
